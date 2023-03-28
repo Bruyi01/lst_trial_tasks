1a226c31116b071e71af45edf88fae4f0bd43e5a
Bruiyi01 <bruevich@mail.ru>
2023-03-28 13:29:27 +0300

    0.0.4 [*] client data`s collection is reworked; [+] add data`s file filling

--- hosts_scanner.py
+++ hosts_scanner.py
@@ -1,7 +1,5 @@
 import asyncio
 import json
-import os
-import paramiko
 import sys
 import asyncssh
 
@@ -12,67 +10,38 @@ class AsyncTask():
         self.hosts_data = None
 
     def readFromJsonFile(self):
-        hosts_data = json.loads('hosts.json')
-        return hosts_data['hosts']
+        return json.loads('hosts.json')['hosts']
 
-    async def takeGitDataFromClient(self, server, user, userpass):
+    async def takeDataFromClient(self, server, user, userpass):
         async with asyncssh.connect(server, username=user, password=userpass) as conn:
             result = (await conn.run('cd ~/bw/ ; git branch -vv', check=True)).split(' ')[:2]
             if result.startwith('fatal'):
-                result = await conn.run('cd ~/bw/ ; svn info', check=True)
-                """
-                добавить фильтр на входные значения
-                """
-            repos_data_branch = {'branch': result.split(' ')[0]}
-            repos_data_revision = {'revision': result.split(' ')[1]}
-        return repos_data_branch, repos_data_revision
-
-    async def takeSubVersionDatafromClient(self, server, user, userpass):
-        async with asyncssh.connect(server, username=user, password=userpass) as conn:
-            result = await conn.run('cd ~/bw/ ; svn info', check=True)
-            return result
-
-    ...
-
-
-"""
-    async def sshClient(self, host, username, passwd, command):
-        async with asyncssh.connect(host, username=username, password=passwd, known_hosts=None) as conn:
-            return await conn.run(command, check=True)
-
-    async def multi_client(self, list_input):
-        hosts = list_input
-
-        tasks = (AsyncTask.sshClient(host, 'show ver') for host in hosts)
-        results = await asyncio.gather(*tasks, return_exceptions=True)
-
-        for dev, result in enumerate(results, 1):
-            file = open(hosts[dev - 1] + '.json', 'w+')
-            if isinstance(result, Exception):
-                print('Task %d failed: %s' % (dev, str(result)))
-            elif result.exit_status != 0:
-                print('Task %d exited with status %s:' % (dev, result.exit_status))
-                print(result.stderr, end='')
+                client_data_branch = await conn.run('cd ~/bw/ ; svn info | grep ^Revision',
+                                                    check=True).split(': ')[1]
+                client_data_revision = await conn.run('cd ~/bw/ ; svn info | grep ^Last Changed Rev',
+                                                      check=True).split(': ')[1]
             else:
-                print('Task %d succeeded for device %s:' % (dev, hosts[dev - 1]))
-                # print(result.stdout + '\n', end='')
-                file.write(result.stdout + '\n')
+                client_data_branch = {'branch': result.split(' ')[0]}
+                client_data_revision = {'revision': result.split(' ')[1]}
+        return client_data_branch, client_data_revision
 
-            print(25 * '#')
-"""
+    def fillDataFromHosts(self):
+        hosts_data = self.readFromJsonFile()
+        try:
+            for item in hosts_data:
+                branch, revision = asyncio.get_event_loop().run_until_complete(self.takeDataFromClient(item['host'],
+                                                                                                       item['user'],
+                                                                                                       item['password']))
+                item.set(branch)
+                item.set(revision)
+        except (OSError, asyncssh.Error) as exc:
+            sys.exit(f'SSH connection failed: {str(exc)}')
+        with open('hosts.json', 'w') as full_data_file:
+            full_data_file.write(hosts_data)
+            full_data_file.close()
 
 
 def main():
-    """
-    devs = ['dev1', 'dev2']
-    cmds = ['show mod', 'show ver']
-
-    passwd = getpass.getpass("passwd: ")
-    startTime = datetime.now()
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(AsyncTask.multi_client(devs))
-    print('\nElapsed: ', datetime.now() - startTime)
-    """
 
     ...
 
