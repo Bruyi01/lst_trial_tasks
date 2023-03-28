1c2dd139d7561d3299135475de9cc08262ace151
Bruiyi01 <bruevich@mail.ru>
2023-03-28 13:59:54 +0300

    0.0.5 [+] add README.md file; [+] add main function; [+] add check hosts_file.json existing

---
+++ README.md
@@ -0,0 +1,19 @@
+# Lesta test task by __Bruyi__
+
+---
+### This project was created for test challenge
+
+---
+
+## Requirements:
+
+---
+**asyncssh**
+
+**asyncio**
+
+**json**
+
+**python 3.10**
+
+---
--- hosts_scanner.py
+++ hosts_scanner.py
@@ -2,23 +2,27 @@ import asyncio
 import json
 import sys
 import asyncssh
+import os.path
 
 
-class AsyncTask():
+class AsyncTask:
 
     def __init__(self):
         self.hosts_data = None
 
     def readFromJsonFile(self):
+        if os.path.isfile('hosts.json') is False:
+            print('server doesn`t have a "hosts.json" file in project`s directory, please insert file and re-run script')
+            exit()
         return json.loads('hosts.json')['hosts']
 
     async def takeDataFromClient(self, server, user, userpass):
-        async with asyncssh.connect(server, username=user, password=userpass) as conn:
+        async with asyncssh.connect(server, username=user, password=userpass, known_hosts=None) as conn:
             result = (await conn.run('cd ~/bw/ ; git branch -vv', check=True)).split(' ')[:2]
             if result.startwith('fatal'):
                 client_data_branch = await conn.run('cd ~/bw/ ; svn info | grep ^Revision',
                                                     check=True).split(': ')[1]
-                client_data_revision = await conn.run('cd ~/bw/ ; svn info | grep ^Last Changed Rev',
+                client_data_revision = await conn.run("cd ~/bw/ ; svn info | grep ^'Last Changed Rev'",
                                                       check=True).split(': ')[1]
             else:
                 client_data_branch = {'branch': result.split(' ')[0]}
@@ -31,7 +35,7 @@ class AsyncTask():
             for item in hosts_data:
                 branch, revision = asyncio.get_event_loop().run_until_complete(self.takeDataFromClient(item['host'],
                                                                                                        item['user'],
-                                                                                                       item['password']))
+                                                                                                       item['user']))
                 item.set(branch)
                 item.set(revision)
         except (OSError, asyncssh.Error) as exc:
@@ -42,9 +46,8 @@ class AsyncTask():
 
 
 def main():
-
-    ...
-
+    AT = AsyncTask()
+    AT.fillDataFromHosts()
 
 if __name__ == '__main__':
     main()
